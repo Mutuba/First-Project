f158bbef5d9baf31c076e4193c7781d5f0c6e6c5
danielmutuba <danielmutubait@gmail.com>
2018-05-17 11:38:47 +0300

    Improved file readabilty

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask import request
- 
+
 app = Flask(__name__)
- 
-@app.route('/postjson', methods = ['POST'])
+
+
+@app.route('/postjson', methods=['POST'])
 def postJsonHandler():
-    print (request.is_json)
+    print(request.is_json)
     content = request.get_json()
-    print (content)
+    print(content)
     return 'JSON posted'
- 
+
+
 app.run(host='0.0.0.0', port= 8090)
+
