059ccb57507f5e386d376a1a794615de31634a8a
danielmutuba <danielmutubait@gmail.com>
2018-06-05 12:28:55 +0300

    added data.py [finishes #

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+	{ 'id' : 1, 
+	  'title' : 'Article One',
+	  'body': 'Differences in how writing systems represent language raise important questions about the extent to which.',
+	  'Author':'Daniel Mutuba',
+	  'create_date' : '11/04/2017'
+
+	},
+	{ 'id' : 2, 
+	  'title' : 'Article Two',
+	  'body': 'Differences in how writing systems represent language raise important questions about the extent to which.',
+	  'Author':'Janet Kogo',
+	  'create_date' : '11/04/2017'
+
+	},
+	{ 'id' : 3, 
+	  'title' : 'Article Three',
+	  'body': 'Differences in how writing systems represent language raise important questions about the extent to which.',
+	  'Author':'Diana Jerop',
+	  'create_date' : '11/04/2017'
+
+	}
+	]
+	return articles
